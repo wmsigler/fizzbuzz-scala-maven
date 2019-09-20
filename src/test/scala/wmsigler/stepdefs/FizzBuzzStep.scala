fea6154b67d5cfeb243c970e84b1c66c80549fd8
wmsigler <42501717+wmsigler@users.noreply.github.com>
2019-09-19 23:17:05 -0600

    Implement Cucumber step definitions for default scenario

--- src/test/scala/wmsigler/stepdefs/FizzBuzzStep.scala
+++ src/test/scala/wmsigler/stepdefs/FizzBuzzStep.scala
@@ -1,27 +1,45 @@
 package wmsigler.stepdefs
 
-import cucumber.api.PendingException
 import cucumber.api.scala.{EN, ScalaDsl}
 import io.cucumber.datatable.DataTable
+import wmsigler.FizzBuzz.fizzbuzz
+
+import scala.collection.JavaConverters._
+import collection.mutable.{Buffer => MutableBuffer}
 
 class FizzBuzzStep extends ScalaDsl with EN {
+  object DefaultValues extends Enumeration {
+    var FizzVal : Value = Value(3)
+    val BuzzVal : Value = Value(5)
+    val Limit : Value = Value(100)
+  }
+
+  private var fizzVal = DefaultValues.FizzVal.id
+  private var buzzVal = DefaultValues.BuzzVal.id
+  private var limit = DefaultValues.Limit.id
+  private var actual = MutableBuffer[String]()
+
   Given("""no parameters are specified"""){ () =>
-    //// Write code here that turns the phrase above into concrete actions
-    throw new PendingException()
+    fizzVal = DefaultValues.FizzVal.id
+    buzzVal = DefaultValues.BuzzVal.id
+    limit = DefaultValues.Limit.id
+    actual = MutableBuffer[String]()
   }
 
   When("""FizzBuzz is calculated""") { () =>
-    //// Write code here that turns the phrase above into concrete actions
-    throw new PendingException()
+    for (i <- 1 to limit) {
+      actual += fizzbuzz(i, fizzVal, buzzVal)
+    }
   }
 
   Then("""the first {int} results should be:""") { (numLines: Int, table: DataTable) =>
-    //// Write code here that turns the phrase above into concrete actions
-    throw new PendingException()
+    val expected = table.asList.asScala
+    assert(expected.equals(actual.take(numLines)))
   }
 
   Then("""the last {int} results should be:""") { (numLines: Int, table: DataTable) =>
-    //// Write code here that turns the phrase above into concrete actions
-    throw new PendingException()
+    val first = limit - numLines
+    val expected = table.asList.asScala
+    assert(expected.equals(actual.slice(first, limit)))
   }
 }
